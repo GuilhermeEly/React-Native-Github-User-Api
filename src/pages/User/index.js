f3fa36ef26fcd83e71776ab82f5de1c2dab1c0ac
Guilherme Ely <guilherme.ely.ferreira@gmail.com>
2019-11-28 20:02:41 -0300

    Pagination and LoadingScreen

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/static-property-placement */
 /* eslint-disable react/state-in-constructor */
 import React, { Component } from 'react';
+import { ActivityIndicator } from 'react-native';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 
@@ -16,6 +17,7 @@ import {
   Title,
   OwnerAvatar,
   Author,
+  Loading,
 } from './styles';
 
 export default class User extends Component {
@@ -31,20 +33,42 @@ export default class User extends Component {
 
   state = {
     stars: [],
+    loadingStarredRepo: true,
+    page: 1,
   };
 
   async componentDidMount() {
+    this.loadStarred();
+  }
+
+  loadStarred = async (page = 1) => {
     const { navigation } = this.props;
+    const { stars } = this.state;
+
     const user = navigation.getParam('user');
 
-    const response = await api.get(`/users/${user.login}/starred`);
+    const response = await api.get(`/users/${user.login}/starred`, {
+      params: { page },
+    });
 
-    this.setState({ stars: response.data });
-  }
+    this.setState({
+      stars: page >= 2 ? [...stars, ...response.data] : response.data,
+      loadingStarredRepo: false,
+      page,
+    });
+  };
+
+  loadMore = () => {
+    const { page } = this.state;
+
+    const nextPage = page + 1;
+
+    this.loadStarred(nextPage);
+  };
 
   render() {
     const { navigation } = this.props;
-    const { stars } = this.state;
+    const { stars, loadingStarredRepo } = this.state;
     const user = navigation.getParam('user');
     return (
       <Container>
@@ -53,20 +77,27 @@ export default class User extends Component {
           <Name>{user.name}</Name>
           <Bio>{user.bio}</Bio>
         </Header>
-
-        <Stars
-          data={stars}
-          keyExtractor={star => String(star.id)}
-          renderItem={({ item }) => (
-            <Starred>
-              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
-              <Info>
-                <Title>{item.name}</Title>
-                <Author>{item.owner.login}</Author>
-              </Info>
-            </Starred>
-          )}
-        />
+        {loadingStarredRepo ? (
+          <Loading>
+            <ActivityIndicator size="large" color="#715981" />
+          </Loading>
+        ) : (
+          <Stars
+            data={stars}
+            onEndReachedThreshold={0.2} // Carrega mais itens quando chegar em 20% do fim
+            onEndReached={this.loadMore}
+            keyExtractor={star => String(star.id)}
+            renderItem={({ item }) => (
+              <Starred>
+                <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            )}
+          />
+        )}
       </Container>
     );
   }
